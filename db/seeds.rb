2077554094acfd099acfc3a2fec72de77616125d
TzuYuan <tzuyuanliu0830@gmail.com>
2016-06-17 16:43:21 +0800

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create!( :name => "Shelter information")
+Category.create!( :name => "Support Stuff")
+Category.create!( :name => "Ask for Help")
